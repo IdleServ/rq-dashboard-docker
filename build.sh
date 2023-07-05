909978dd5c269e17943b5f033bdac61b1655354b
Chris Sheppard <chris@idleserv.net>
2023-07-05 21:25:33 +0100

    Version 1.0.8

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -ex
 USERNAME=idleserv
 IMAGE=rq-dashboard
 
-docker buildx build --platform linux/amd64,linux/arm64 -t $USERNAME/$IMAGE:latest .
+docker buildx build --platform linux/amd64 -t $USERNAME/$IMAGE:latest .
